7f105f26f5be2646cc469b0188231ce3c28622fe
damn21 <dmezae@uninorte.edu.co>
2022-04-05 13:02:32 -0500

    feat: 🔨 random_words method added

--- lib/random_words.dart
+++ lib/random_words.dart
@@ -10,7 +10,10 @@ class RandomWords extends StatefulWidget {
 class _RandomWordsState extends State<RandomWords> {
   String _theState = "0";
   int _actualWordType = 0;
+  int _score = 0;
+  var _colorState = Colors.blue;
   final _random = new Random();
+  bool _isEnabled = true;
 
   int next(int min, int max) => min + _random.nextInt(max - min);
 
@@ -20,44 +23,148 @@ class _RandomWordsState extends State<RandomWords> {
     setRandomWord();
   }
 
+  void _enableButton() {
+    setState(() {
+      _isEnabled = true;
+    });
+  }
+
+  void _disableButton() {
+    setState(() {
+      _isEnabled = false;
+    });
+  }
+
+  void _changeword(String word) {
+    setState(() {
+      _theState = word;
+    });
+  }
+
+  void _changeColorToRed() {
+    setState(() {
+      _colorState = Colors.red;
+    });
+  }
+
+  void _changeColorToBlue() {
+    setState(() {
+      _colorState = Colors.blue;
+    });
+  }
+
+  void _resetScore() {
+    setState(() {
+      _score = 0;
+    });
+  }
+
+  void _incrementScore() {
+    setState(() {
+      _score++;
+    });
+  }
+
   void setRandomWord() {
     var option = next(0, 2);
     var randomItem = "";
     if (option == 0) {
       print("change to noun");
       randomItem = (nouns.toList()..shuffle()).first;
+      _changeword(randomItem);
+      _actualWordType = 1;
     } else {
       print("change to adjective");
       randomItem = (adjectives.toList()..shuffle()).first;
+      _changeword(randomItem);
+      _actualWordType = 2;
     }
   }
 
-  void _onPressed() {}
+  Future<void> _onPressed(int type) async {
+    if (type == _actualWordType) {
+      _incrementScore();
+      setRandomWord();
+    } else {
+      if (_score > 0) {
+        _changeColorToRed();
+        _disableButton();
+        await Future.delayed(const Duration(seconds: 3));
+        setState(() {
+          _score--;
+        });
+        setRandomWord();
+        _changeColorToBlue();
+        _enableButton();
+      } else {
+        _changeColorToRed();
+        _disableButton();
+        await Future.delayed(const Duration(seconds: 3));
+        setRandomWord();
+        _changeColorToBlue();
+        _enableButton();
+      }
+    }
+  }
 
-  void _onReset() {}
+  void _onReset() {
+    setRandomWord();
+    _resetScore();
+    _changeColorToBlue();
+    _enableButton();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Random Words"),
+        //title: Text("Random Words"),
+        title:
+            const Text('Random Words', style: TextStyle(color: Colors.black)),
+        backgroundColor: Colors.yellow,
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Text("$_theState"),
+          Row(mainAxisAlignment: MainAxisAlignment.end,
+              //children: [Text("Score: $_score")]),
+              children: [
+                Text('Score: $_score',
+                    style: TextStyle(color: Colors.blue, fontSize: 20))
+              ]),
+          new Text(
+            '$_theState',
+            style: TextStyle(
+              color: _colorState,
+              fontSize: 35,
+            ),
+          ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               ElevatedButton(
-                  onPressed: () => _onPressed(), child: Text("Noun")),
+                onPressed: _isEnabled ? () => _onPressed(1) : null,
+                child: Text('Noun'),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.purple,
+                  onPrimary: Colors.yellow,
+                ),
+              ),
               ElevatedButton(
-                  onPressed: () => _onPressed(), child: Text("Adjective"))
+                  onPressed: _isEnabled ? () => _onPressed(2) : null,
+                  child: Text('Adjective'),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.purple,
+                    onPrimary: Colors.yellow,
+                  )),
             ],
           ),
-          ElevatedButton(onPressed: () => _onReset(), child: Text("Reset")),
+          Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+            IconButton(icon: Icon(Icons.refresh), onPressed: () => _onReset())
+          ]),
         ],
       ),
     );
   }
 }
+
